50c024e86829ef8877b6dc11d2fc8b9d593216a9
Nicholas Smart <160883116+NickSmart14@users.noreply.github.com>
2025-05-06 11:31:38 +0200

    Added More to the rocket boost pickup

--- EndlessRunner/Assets/Scripts/Pickups/Rocket Pickup/RocketEffect.cs
+++ EndlessRunner/Assets/Scripts/Pickups/Rocket Pickup/RocketEffect.cs
@@ -1,16 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "PowerUp/RocketPickup")]
 public class RocketEffect : PowerUp_Effect
 {
+ 
     public override void ApplyEffect(GameObject target)
     {
         target.GetComponent<PlayerController>().maxSpeed = 150;
+        target.GetComponent<PlayerDeath>().PlayerImmune = true;
+
+      
+
+   
+  
+     
+
     }
 
     public override void DisableEffect(GameObject target)
     {
         target.GetComponent<PlayerController>().maxSpeed = 75;
+        target.GetComponent<PlayerDeath>().PlayerImmune = false;
+     
+
     }
 }
     
--- EndlessRunner/Assets/Scripts/Player/PlayerDeath.cs
+++ EndlessRunner/Assets/Scripts/Player/PlayerDeath.cs
@@ -4,19 +4,22 @@ public class PlayerDeath : MonoBehaviour
 {
     private PlayerController playerController;
     public DeathScreen deathScreen;
+    public bool PlayerImmune;
+
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerController  = gameObject.GetComponent<PlayerController>();        //get the player's controller script
+        playerController = gameObject.GetComponent<PlayerController>();        //get the player's controller script
     }
 
 
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Obstacle"))        //if the collision that the player had is with an obstacle
+        if (collision.gameObject.CompareTag("Obstacle") && !PlayerImmune)        //if the collision that the player had is with an obstacle
         {
             if (this != null)
             {
@@ -24,6 +27,18 @@ public class PlayerDeath : MonoBehaviour
             }
             deathScreen.ShowDeathScreen();              //show the deathscreen
         }
-    }
 
+        if (collision.gameObject.CompareTag("Obstacle") && PlayerImmune)
+        {
+           
+            if (this != null)
+            {
+                Destroy(collision.gameObject); // Destroys Obstacles instead when boost effect is active
+             
+            }
+
+
+        }
+
+    }
 }
