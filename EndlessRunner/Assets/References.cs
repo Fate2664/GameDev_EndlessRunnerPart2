1609ac15f6f905c6e091ee18b70cb338eb3c7a59
Nicholas Smart <160883116+NickSmart14@users.noreply.github.com>
2025-05-18 12:02:31 +0200

    Added References

--- EndlessRunner/Assets/References.cs
+++ EndlessRunner/Assets/References.cs
@@ -2,4 +2,22 @@
  * https://www.youtube.com/watch?v=NiOGWZXBg4Y
  * https://www.youtube.com/watch?v=qE6kRTOKzU0
  * 
+ * ARgentics. n.d. Low Poly Buildings 3d Model. [Online] Available at: https://dribbble.com/shots/3662093-Low-Poly-Buildings-3d-Model [Accessed15 May 2025].
+How to Make Low Poly Nature (Blender Tutorial). 2021. YouTube Video, added by Ryan King Art. [Online]. Available at: https://www.youtube.com/watch?v=a2vhQ6GfXIw&t=1s [Accessed 17 May 2025].
+
+How to make a lowpoly truck in blender | part1 |. 2020. YouTube Video, added by LNmaster2008. [Online]. Available at: https://www.youtube.com/watch?v=B4wMoa5y3xU [Accessed10 May 2025].
+
+How to make a lowpoly truck in blender | part2 |. 2020. YouTube Video, added by LNmaster2008. [Online]. Available at: https://www.youtube.com/watch?v=dn8rXrvJNnc Accessed 10 May 2025].
+
+How to make a lowpoly truck in blender | part3 |. 2020. YouTube Video, added by LNmaster2008. [Online]. Available at: https://www.youtube.com/watch?v=_JslkZ2GyZk Accessed 10 May 2025].
+
+Yellow Images. n.d. Low Poly Police Car. [Online] Available at: https://yellowimages.com/stock/low-poly-police-car-yi3602157?ca=1_15 [Accessed 14 May 2025].
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
